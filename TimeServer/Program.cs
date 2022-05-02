c6cc06c63423bff832d687a4a671dabc72ca12cb
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-05-02 09:28:00 +0200

    Add tcpListner

--- TimeServer/Program.cs
+++ TimeServer/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeServer
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var endPoint = new IPEndPoint(IPAddress.Loopback, 44444);
+            var tcpListner = new TcpListener(endPoint);
+            
+            tcpListner.Start();
+            while (true)
+            {
+                byte[] buffer = new byte[100];
+                
+                var tcpClient = tcpListner.AcceptTcpClient();
+                tcpClient.GetStream().Read(buffer, 0, 100);
+                
+                Console.WriteLine("Hello, this is Sokratis Time Server. The Current time is: " 
+                                  + Encoding.ASCII.GetString(buffer));
+                
+                tcpClient.Close();
+            }
+        }
+    }
+};
+
