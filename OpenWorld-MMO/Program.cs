d3bac4ece22c609606347158a7f8b2e891ea3142
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-05-03 15:30:18 +0200

    Add basic UDP server

---
+++ OpenWorld-MMO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace OpenWorld_MMO
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Any, 44444);
+            var udpClient = new UdpClient(ipEndPoint);
+            
+            Console.WriteLine("Waiting client...");
+
+            var sender = new IPEndPoint(IPAddress.Any, 0);
+            var data = udpClient.Receive(ref sender);
+            
+            Console.WriteLine($"Message receiver from{sender}: ");
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+            string message = "Welcome to my server";
+            data = Encoding.ASCII.GetBytes(message);
+            udpClient.Send(data, data.Length, sender);
+        }
+    }    
+};
+
