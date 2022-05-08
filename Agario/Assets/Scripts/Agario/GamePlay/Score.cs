facf24d83dffbc46d179de0edc2ebd40476a482d
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-05-08 17:18:41 +0200

    Add Scale

--- Agario/Assets/Scripts/Agario/GamePlay/Score.cs
+++ Agario/Assets/Scripts/Agario/GamePlay/Score.cs
@@ -6,6 +6,7 @@ namespace Agario
     public class Score : MonoBehaviour, IPoints
     {
         [SerializeField] private int startPoints = 2;
+        [SerializeField] private float scaleRate = 0.1f;
         
         public int Points { get; set; }
 
@@ -14,8 +15,13 @@ namespace Agario
         private void OnCollisionEnter2D(Collision2D col)
         {
             var otherPoints = col.gameObject.GetComponent<IPoints>();
-            
-            if (otherPoints != null && Points > otherPoints.Points) Points += otherPoints.Points;
+
+            if (otherPoints != null && Points > otherPoints.Points)
+            {
+                Points += otherPoints.Points;
+                var scale = otherPoints.Points * scaleRate;
+                transform.localScale += new Vector3(scaleRate , scaleRate , 0);
+            }
             else Destroy(gameObject);
             
             Debug.Log(Points);
