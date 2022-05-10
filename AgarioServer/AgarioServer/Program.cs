64d6a5b26d6ed61d35be9395a4740617b531a16e
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-05-10 16:03:39 +0200

    Add AgarioServer

---
+++ AgarioServer/AgarioServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AgarioServer
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var endPoint = new IPEndPoint(IPAddress.Any, 44444);
+            var udpClient = new UdpClient(endPoint);
+            Console.WriteLine("Server is running");
+
+            while (true)
+            {
+                var sender = new IPEndPoint(IPAddress.Any, 0);
+                var data = udpClient.Receive(ref sender);
+                
+                Console.WriteLine(udpClient + "connected. Message: " + Encoding.ASCII.GetString(data));
+            }
+        }
+    }
+}
