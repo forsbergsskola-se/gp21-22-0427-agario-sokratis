ff7ba9441f3a7e9deb80330dc794601901e95fe1
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-05-11 15:44:46 +0200

    Add CoinPool

---
+++ Agario/Assets/Scripts/Agario/GamePlay/Stage/CoinsPool.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Agario.ScriptableObjects;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Agario.GamePlay.Stage
+{
+    public class CoinsPool : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private GameObject coin;
+        [SerializeField] private int size;
+        [SerializeField] private float timer;
+
+        [Header("Dependencies")]
+        [SerializeField] private IntValue stageSize;
+        
+        private Queue<GameObject> pool;
+
+        private void Awake()
+        {
+            pool = new Queue<GameObject>();
+            FillPool();  
+        } 
+
+        private void Start() => StartCoroutine(SpawnCoin());
+        
+        private IEnumerator SpawnCoin()
+        {
+            var item = pool.Dequeue();
+            item.transform.position = SetRandomPosition(stageSize.Value);
+            pool.Enqueue(item);
+            
+            yield return new WaitForSeconds(timer);
+            StartCoroutine(SpawnCoin());
+        }
+
+        private void FillPool()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                pool.Enqueue(Instantiate(coin)); 
+            }
+            Debug.Log(pool.Count);
+        }
+        
+        private Vector3 SetRandomPosition(int threshold) 
+            => new (Random.Range(stageSize.Value * -1, stageSize.Value), 
+                    Random.Range(stageSize.Value * -1, stageSize.Value), 
+                    0);
+    }
+}
