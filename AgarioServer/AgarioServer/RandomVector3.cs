82bdda70283ce3233ca4f396382c4fb3f40beced
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-05-17 13:10:41 +0200

    Refactor RandomVector3

--- AgarioServer/AgarioServer/RandomVector3.cs
+++ AgarioServer/AgarioServer/RandomVector3.cs
@@ -4,17 +4,17 @@ namespace AgarioServer
 {
     public class RandomVector3
     {
-        public float X { get; }
-        public float Y { get; }
-        public float Z { get; }
+        public float x { get; }
+        public float y { get; }
+        public float z { get; }
 
         public RandomVector3(int range)
         {
             var random = new Random();
 
-            X = random.Next(range * -1, range);
-            Y = random.Next(range * -1, range);
-            Z = random.Next(range * -1, range);
+            x = random.Next(range * -1, range);
+            y = random.Next(range * -1, range);
+            z = random.Next(range * -1, range);
         }
     }
 }
